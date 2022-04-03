ed1764cf2cf57d6b961c38e379809d7f716effb4
Victor Neves <info@victorneves.dev>
2022-04-03 16:46:16 +0100

    New test to get component name without extra script tag

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import components from './components'
 
 const plugin = {
   install(Vue) {
-    console.log('[vuepress-plugin-dekp] install', components)
+    // console.log('[vuepress-plugin-dekp] install', components)
     for (const prop in components) {
-      console.log('prop', prop)
+      console.log('prop', prop, typeof prop)
       if (prop !== undefined) {
         // const component = components[prop]
         Vue.component(prop, prop)
